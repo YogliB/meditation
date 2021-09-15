9c961100c338c05143f6c6d5bc25d70c46b71740
YogliB <me@yogev.dev>
2021-09-15 15:11:50 +0300

    fix: make fonts load faster

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,14 +3,6 @@
 	import { Navbar } from '$lib/components/molecules';
 </script>
 
-<svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Glory&display=swap"
-		rel="stylesheet"
-	/>
-</svelte:head>
 
 <Navbar />
 
