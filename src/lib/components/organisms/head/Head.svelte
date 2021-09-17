2ea856bfd3ad902e4608d4e07ac1bb6160b5f59c
YogliB <me@yogev.dev>
2021-09-17 10:58:21 +0300

    feat: add analytics

--- src/lib/components/organisms/head/Head.svelte
+++ src/lib/components/organisms/head/Head.svelte
@@ -15,4 +15,11 @@
 	<meta charset="utf-8" />
 	<link rel="icon" href="/favicon.png" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<script
+		async
+		defer
+		data-website-id="5e3b43de-968a-41dd-a29a-9bdf70522572"
+		src="https://umami.yogev.dev/umami.js"
+		data-do-not-track="true"
+		data-domains="yogev.dev"></script>
 </svelte:head>
