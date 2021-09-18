081cb7c9fc12d0f62447084ebb8ab04485cf3bcc
YogliB <me@yogev.dev>
2021-09-18 10:57:06 +0300

    feat: add ananytics link

--- src/lib/components/organisms/footer/Footer.svelte
+++ src/lib/components/organisms/footer/Footer.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-	import { faHeart } from '@fortawesome/free-solid-svg-icons';
+	import { faHeart, faChartLine } from '@fortawesome/free-solid-svg-icons';
 	import { faGithub } from '@fortawesome/free-brands-svg-icons';
 	import { Icon, Link } from '$lib/components/atoms';
 </script>
 
 <footer class="flex justify-between items-center p-4">
-	<Link
-		href="https://github.com/YogliB/meditation"
-		icon={faGithub}
-		iconSize="large"
-		label="Github"
-		hideLabel={true}
-	/>
+	<div class="flex items-center gap-4">
+		<Link
+			href="https://github.com/YogliB/meditation"
+			icon={faGithub}
+			iconSize="large"
+			label="Github"
+			hideLabel={true}
+		/>
+		<Link
+			href="https://umami.yogev.dev/share/OUVRwjxV/there"
+			icon={faChartLine}
+			iconSize="large"
+			label="Analytics"
+			hideLabel={true}
+		/>
+	</div>
 
 	<div>
 		Made with <Icon icon={faHeart} color="danger" /> by
