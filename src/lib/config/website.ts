a1e162aa3f3c225ee0c3aca5e0796a0c5f115a33
YogliB <me@yogev.dev>
2021-09-18 11:17:29 +0300

    refactor: simplify seo settings

--- src/lib/config/website.ts
+++ src/lib/config/website.ts
@@ -1,20 +1,6 @@
 export const website = {
-	author: 'Yogev Boaron Ben-Har',
-	entity: 'there',
 	siteKeywords:
 		'meditation, timer, clock, simple, open-source, libre, free-software, web-app, web',
-	ogLanguage: 'en_US',
-	siteLanguage: 'en-US',
 	siteTitle: 'there - be there for yourself',
 	siteDescription: 'Simple and easy-to-use meditation timer.',
-	siteShortTitle: 'Meditation timer',
-	siteUrl: import.meta.env.VITE_SITE_URL,
-	contactEmail: import.meta.env.VITE_CONTACT_EMAIL,
-	facebookAuthorPage: `https://www.facebook.com/${
-		import.meta.env.VITE_FACEBOOK_AUTHOR_PAGE
-	}`,
-	githubPage: import.meta.env.VITE_GITHUB_PAGE,
-	linkedinProfile: import.meta.env.VITE_LINKEDIN_PROFILE,
-	twitterUsername: import.meta.env.VITE_TWITTER_USERNAME,
-	twitterUserId: import.meta.env.VITE_TWITTER_ID,
 };
