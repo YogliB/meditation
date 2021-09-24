2dd72bd26d4e41adc57596a8066c15cd3d9c7344
YogliB <me@yogev.dev>
2021-09-24 16:36:13 +0300

    refactor: improve seo

--- src/lib/config/website.ts
+++ src/lib/config/website.ts
@@ -1,6 +1,6 @@
 export const website = {
 	siteKeywords:
-		'meditation, timer, clock, simple, open-source, libre, free-software, web-app, web',
+		'meditation timer, online meditation timer, open-source meditation timer, libre meditation timer',
 	siteTitle: 'there - be there for yourself',
 	siteDescription: 'Simple and easy-to-use meditation timer.',
 };
